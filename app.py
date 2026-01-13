3c002166fee2edd3ad33c791852158787987cdcf
xuandunghungan-sys <xuandunghungan@gmail.com>
2026-01-13 16:52:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -191,7 +191,7 @@ st.markdown("""
 st.markdown("""
 <div class="main-header">
     <h1>📘 TRỢ LÝ SOẠN GIÁO ÁN TỰ ĐỘNG (NLS)</h1>
-    <p>Tác giả: Lù Seo Sần - Trường PTDTBT Tiểu học Bản Ngò - ĐT: 097 1986 343</p>
+    <p>Tác giả: Nghiêm Xuân Dũng - Trường  Tiểu học Hùng An - ĐT: 0912184585 </p>
 </div>
 """, unsafe_allow_html=True)
 
@@ -347,4 +347,4 @@ if st.button("🚀 SOẠN GIÁO ÁN NGAY"):
 
 # --- CHÂN TRANG ---
 st.markdown("---")
-st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Lù Seo Sần - Trường PTDTBT Tiểu học Bản Ngò - ĐT: 097 1986 343</div>", unsafe_allow_html=True)
+st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Nghiêm Xuân Dũng - Trường tiểu học Hùng an- ĐT: 0912184585 </div>", unsafe_allow_html=True)
